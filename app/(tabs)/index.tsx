60e62acd8c81e6edadc64b350e550f2c0b81f1a8
CRodrigo47 <christian3884@gmail.com>
2025-01-14 01:48:59 +0100

    Creacion del componente ImageView y de Button.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,24 @@
 import { Text, View, StyleSheet } from "react-native";
 import { Link } from "expo-router";
+import { Image } from "expo-image";
+import ImageViewer from "@/components/ImageViewer";
+import Button from "@/components/Button";
+
+const PlaceholderImage = require("@/assets/images/background-image.png");
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center" style={styles.container}>
-      <Text className="color-red-200">
-        Home Screen
-      </Text>
-    <Link href="/about" className="text-lg color-white underline">Go to about screen</Link>
+    <View
+      className="flex-1 items-center justify-center"
+      style={styles.container}
+    >
+      <View className="flex-1 pt-7">
+        <ImageViewer imgSource={PlaceholderImage}></ImageViewer>
+      </View>
+      <View className="items-center" style={styles.footerContainer}>
+        <Button label="Choose a photo" theme="primary" />
+        <Button label="Use this photo" />
+      </View>
     </View>
   );
 }
@@ -15,5 +26,8 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "#25292e",
+  },
+  footerContainer: {
+    flex: 1 / 3,
   },
 });
